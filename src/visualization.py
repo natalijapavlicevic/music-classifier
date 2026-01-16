a9b2285f54d9194c5ad5dc1e4e740a0a12afe958
tamara <mi22096@alas.matf.bg.ac.rs>
2026-01-16 22:35:46 +0100

    Spectrogrames done

--- src/visualization.py
+++ src/visualization.py
@@ -3,18 +3,18 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 
-path = 'data/raw/genres_original/jazz/jazz.00000.wav'
+path = 'data/raw/genres_original/rock/rock.00022.wav'
 
 y, sr= librosa.load(path, sr=22050)
 
 plt.figure(figsize=(12,4))
 librosa.display.waveshow(y,sr=sr)
-plt.title('Waveform (Jazz)')
+plt.title('Waveform (Rock)')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.tight_layout()
-plt.savefig('figures/mel_spectrogram_jazz.png', dpi=300)
-plt.close()
+#plt.savefig('figures/rock/waveform_rock.png', dpi=300)
+#plt.close()
 
 
 mel = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
@@ -28,16 +28,16 @@ librosa.display.specshow(
     y_axis='mel'
 )
 plt.colorbar(format='%+2.0f dB')
-plt.title('Mel-spectrogram (Jazz)')
+plt.title('Mel-spectrogram (Rock)')
 plt.tight_layout()
-plt.savefig('figures/mel_spectrogram_jazz2.png', dpi=300)
+plt.savefig('figures/rock/mel_spectrogram_rock5.png', dpi=300)
 plt.close()
 
 
 
 genres = {
-    'Classical': 'data/raw/genres_original/classical/classical.00000.wav',
-    'Metal': 'data/raw/genres_original/metal/metal.00000.wav'
+    'Rock': 'data/raw/genres_original/rock/rock.00022.wav',
+    'Blues': 'data/raw/genres_original/blues/blues.00000.wav'
 }
 
 plt.figure(figsize=(14, 6))
@@ -58,7 +58,7 @@ for i, (genre, path) in enumerate(genres.items()):
     plt.title(f'Mel-spectrogram ({genre})')
 
 plt.tight_layout()
-plt.savefig('figures/classicvsmetal.png', dpi=300)
+plt.savefig('figures/compare/rockvsblues.png', dpi=300)
 plt.close()
 
 
